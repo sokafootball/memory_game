2180bbc60235989935d006e9b4ce5e76ac28c0d7
Laurent Capello <laurent.capello@gmail.com>
2020-10-05 20:22:55 +0200

    Updates

---
+++ precache-manifest.4d6b6b4612c7bccb4d3e51790329052a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8086d772b70e7f65c6c5",
+    "url": "/sokafootball/memory_game/static/css/main.cfe87b70.chunk.css"
+  },
+  {
+    "revision": "8086d772b70e7f65c6c5",
+    "url": "/sokafootball/memory_game/static/js/main.8086d772.chunk.js"
+  },
+  {
+    "revision": "a9878cf5e44de7022127",
+    "url": "/sokafootball/memory_game/static/js/1.a9878cf5.chunk.js"
+  },
+  {
+    "revision": "15750f45bbcf9716a390",
+    "url": "/sokafootball/memory_game/static/js/runtime~main.15750f45.js"
+  },
+  {
+    "revision": "ddb8e4eb2f699a87b1449e91e8f105d5",
+    "url": "/sokafootball/memory_game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sokafootball/memory_game/precache-manifest.4d6b6b4612c7bccb4d3e51790329052a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sokafootball/memory_game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
